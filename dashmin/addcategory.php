cd2e0d2106d647173491c35cac56c4288990fd97
farihafari <farihafari594@gmail.com>
2024-09-11 21:16:10 +0500

    category add

--- dashmin/addcategory.php
+++ dashmin/addcategory.php
@@ -6,19 +6,19 @@ include("components/header.php");
                 <div class="row vh-100 bg-light rounded">
                     <div class="col-md-12">
                         <h3 class="py-3">Add Categories</h3>
-                        <form class="mx-3">
+                        <form class="mx-3" method="post" enctype="multipart/form-data">
                                 <div class="mb-3">
                                     <label for="exampleInputEmail1" class="form-label">category name</label>
                                     <input type="text" class="form-control" id="exampleInputEmail1"
-                                        aria-describedby="emailHelp">
+                                        aria-describedby="emailHelp" name="catName">
                                    
                                 </div>
                                 <div class="mb-3">
                                     <label for="exampleInputPassword1" class="form-label">category image</label>
-                                    <input type="file" class="form-control" id="exampleInputPassword1">
+                                    <input type="file" class="form-control" id="exampleInputPassword1" name="catImage">
                                 </div>
                                
-                                <button type="submit" class="btn btn-primary">Add Category</button>
+                                <button type="submit" class="btn btn-primary" name="addCategory">Add Category</button>
                             </form>
                     </div>
                 </div>
--- dashmin/php/query.php
+++ dashmin/php/query.php
@@ -1,3 +1,21 @@
 <?php
 include("connection.php");
+if(isset($_POST['addCategory'])){
+    $catName = $_POST['catName'];
+    $catImageName= $_FILES['catImage']['name'];
+    $catTmpName = $_FILES['catImage']['tmp_name'];
+    $extension = pathinfo($catImageName,PATHINFO_EXTENSION);
+    $imagPath = 'img/categories/'.$catImageName;
+    if($extension == "jpg" || $extension == "png" || $extension == "jpeg" || $extension == "webp"){
+        if(move_uploaded_file($catTmpName,$imagPath)){
+            $query = $pdo -> prepare("insert into categories(catName,catImage) values(:pn , :pi)");
+            $query->bindParam("pn",$catName);
+            $query->bindParam("pi",$catImageName);
+            $query->execute();
+            echo "<script>
+            alert('category add successfully')
+            </script>";
+        }
+    }
+}
 ?>
